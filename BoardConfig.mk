6f76c4a884f3fd00b5a3c77f245de10d578ce279
houst0nn <houstonn@gmail.com>
2014-05-04 18:37:26 -0400

    find7a: drop hwui, use better gfx config values
    Change-Id: I7e3ce3830d11fef20710bf14d0275c023ff5285c
    Signed-off-by: houst0nn <houstonn@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -133,3 +133,13 @@ TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 TW_NO_USB_STORAGE := true
 TW_INCLUDE_JB_CRYPTO := true
 TW_NO_SCREEN_BLANK := true
+
+# Shader cache config options
+# Maximum size of the  GLES Shaders that can be cached for reuse.
+# Increase the size if shaders of size greater than 12KB are used.
+MAX_EGL_CACHE_KEY_SIZE := 12*1024
+
+# Maximum GLES shader cache size for each app to store the compiled shader
+# binaries. Decrease the size if RAM or Flash Storage size is a limitation
+# of the device.
+MAX_EGL_CACHE_SIZE := 2048*1024
