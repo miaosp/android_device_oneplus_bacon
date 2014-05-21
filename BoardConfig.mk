e5339d302a07aef5c1e65d659852e23ba753fddc
Andre Saddler <dresadd09691@gmail.com>
2014-05-21 01:08:33 -0400

    find7a: target board platform gpu should be adreno 330 not adreno 300
    Change-Id: I2abd3cb81f04113f15bf3bf4a14a18b0c5c28a9d
    Signed-off-by: Andre Saddler <dresadd09691@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,7 +15,7 @@ TARGET_KERNEL_CONFIG := msm8974_find7_defconfig
 
 # Platform
 TARGET_BOARD_PLATFORM := msm8974
-TARGET_BOARD_PLATFORM_GPU := qcom-adreno300
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno330
 
 # Architecture
 TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
